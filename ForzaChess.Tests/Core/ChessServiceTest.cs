0f84d97c5b9e77ee472f3c3788ac97ba1963722e
Giacomo Tagliabue <giacomo.tag@gmail.com>
2013-12-22 11:30:14 -0600

    Created ChessServiceTest

---
+++ ForzaChess.Tests/Core/ChessServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ForzaChess.Tests.Core
+{
+    [TestClass]
+    class ChessServiceTest
+    {
+        [TestMethod]
+        public void Match()
+        {
+            
+        }
+    }
+}
