ef14635c6b8226a22bd005db2ee43cff71707573
Giacomo Tagliabue <giacomo.tag@gmail.com>
2014-02-28 16:45:40 -0600

    Add FenParser and its test class

---
+++ ChessCore/Fen/FenParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ForzaChess.Core.Model;
+
+namespace ForzaChess.Core.Fen
+{
+    class FenParser
+    {
+        public Chessboard GenerateBoard(string fen)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ForzaChess.Tests/Core/FenTest.cs
@@ -0,0 +1,15 @@
+﻿using ForzaChess.Core;
+using ForzaChess.Core.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ForzaChess.Tests.Core
+{
+    [TestClass]
+    public class FenTest
+    {
+        [TestMethod]
+        public void GenerateBoardTest()
+        {
+        }
+    }
+}
